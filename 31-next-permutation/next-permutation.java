009a0da61d3fb6259634e23c11afda787de0e013
Anjali Upadhyay <125463983+AnjaliUp@users.noreply.github.com>
2024-12-18 16:17:25 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (51.29%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,47 +1,44 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        
         int n = nums.length;
         
-        // Step 1: Find the break point
         int ind = -1;
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                ind = i;  // break point found
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                ind = i; 
                 break;
             }
         }
         
-        // If no break point exists, reverse the entire array
-        if (ind == -1) {
-            reverse(nums, 0, n - 1);
-            return;
+        if(ind == -1){
+            reverse(nums, 0, n-1);
+            return ;
         }
         
-        // Step 2: Find the next greater element and swap
-        for (int i = n - 1; i > ind; i--) {
-            if (nums[i] > nums[ind]) {
-                swap(nums, i, ind);
+        for(int i=n-1; i>ind; i--){
+            if(nums[i]>nums[ind]){
+                swap(nums, ind, i);
                 break;
             }
         }
         
-        // Step 3: Reverse the right half
-        reverse(nums, ind + 1, n - 1);
+        reverse(nums, ind+1, n-1);
     }
-    
-    // Helper method to reverse a portion of the array
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            swap(nums, start, end);
-            start++;
-            end--;
+        
+        private void reverse(int nums[],int start,int end){
+            while(start<end){
+                swap(nums, start, end);
+                start++;
+                end--;
+            }
         }
+        
+        private void swap(int nums[],int i,int j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        
     }
-    
-    // Helper method to swap two elements in the array
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-}
+
