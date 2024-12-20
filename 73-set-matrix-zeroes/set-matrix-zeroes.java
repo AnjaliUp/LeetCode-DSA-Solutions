1f5af2c26d9febf93f2d36dfeabbe50a9710cbf7
Anjali Upadhyay <125463983+AnjaliUp@users.noreply.github.com>
2024-12-20 23:31:28 +0530

    Time: 1 ms (75.63%) | Memory: 45.8 MB (29.36%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,29 +1,31 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
-
-        boolean[] forRows= new boolean[m];
-        boolean[] forCols= new boolean[n];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==0){
-                    forRows[i]=true;
-                    forCols[j]=true;
-                }
-            }
-        }//we marked the rows and cols with true
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(forRows[i] || forCols[j]){
-                    matrix[i][j]=0;
+        
+       int n = matrix.length;    // Number of rows
+        int m = matrix[0].length; // Number of columns
+        
+        // Step 1: Create two auxiliary arrays to mark rows and columns
+        int[] rowMarker = new int[n];
+        int[] colMarker = new int[m];
+        
+        // Step 2: Traverse the matrix to mark rows and columns
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    rowMarker[i] = 1; // Mark the current row
+                    colMarker[j] = 1; // Mark the current column
                 }
             }
         }
-
-
+        
+        // Step 3: Traverse the matrix again and set cells to 0
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (rowMarker[i] == 1 || colMarker[j] == 1) {
+                    matrix[i][j] = 0; // Set the cell to 0
+                }
+            }
+        }
         
     }
 }
