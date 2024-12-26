3f90b88a50ec6e04f209c1680c710c97f21b0332
Anjali Upadhyay <125463983+AnjaliUp@users.noreply.github.com>
2024-12-27 00:35:47 +0530

    Time: 2 ms (32.60%) | Memory: 42.5 MB (39.62%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,30 +1,32 @@
 class Solution {
     public int myAtoi(String s) {
-        if(s==null) return 0;
-
-        s=s.trim();
         
-        if(s.length()==0) return 0;
+        if(s == null) return 0;
+        s = s.trim();
+        
+        if(s.length() == 0) return 0;
         
         int sign = +1;
         long ans = 0;
-        if(s.charAt(0) == '-') sign = -1;
         
-        int MAX = Integer.MAX_VALUE, MIN = Integer.MIN_VALUE;
-		
-		// initiate the starting pointer
-        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;  // If there is anysign before the start traversal from index 1  else from the index 0
-		
-        while(i < s.length()) {
-            if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) break;
-            ans = ans * 10 + s.charAt(i)-'0';
-            // check the conditions
-            if(sign == -1 && -1*ans < MIN) return MIN;
-            if(sign == 1 && ans > MAX) return MAX;
+        if(s.charAt(0)=='-')sign=-1;
+        int MAX = Integer.MAX_VALUE;
+        int MIN = Integer.MIN_VALUE;
+        
+        int i=(s.charAt(0)=='+' || s.charAt(0)=='-')?1:0;
+        
+        while(i<s.length()){
+            if(s.charAt(i)==' ' || !Character.isDigit(s.charAt(i))) break;
             
-            i++;
+            ans = ans*10 + s.charAt(i)-'0';
+            
+            if(sign==-1 && -1*ans<MIN) return MIN;
+            if(sign==1 && ans>MAX) 
+                return MAX;
+                i++;
         }
         
         return (int)(sign*ans);
+        
     }
 }
